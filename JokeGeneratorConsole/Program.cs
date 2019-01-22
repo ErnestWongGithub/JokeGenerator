0eafa0a9d2b7f3b5725d3e50a3464b066f2e0e44
Ernest Wong <42599143+ErnestWongGithub@users.noreply.github.com>
2019-01-22 14:39:14 -0500

    V 0.0.2 - Loop the full game

--- JokeGeneratorConsole/Program.cs
+++ JokeGeneratorConsole/Program.cs
@@ -4,16 +4,27 @@ namespace JokeGenerator
 {
     class Program
     {
+        // variables
+        private static bool loopGame;
         static UserInput read = new UserInput();
+        static char inputChar;
+
+        private static void DefaultValues()
+        {
+            loopGame = true;
+        }
+
         static void Main(string[] args)
         {
-            char inputChar;
-            StartBanner();
-            inputChar = read.Key();
-            Console.WriteLine(inputChar);
-            // currently line underneath is used to read previous commands
-            inputChar = read.Key();
-            // System.Environment.Exit(1);
+            DefaultValues();
+            while (loopGame == true)
+            {
+                StartBanner();
+                StartCondition();
+                RepeatGame();
+            }
+            
+            System.Environment.Exit(1);
         }
 
         private static void StartBanner()
@@ -25,8 +36,53 @@ namespace JokeGenerator
             Console.WriteLine("Press [esc] at anytime to exit.");
         }
 
-        private static void UserInput(ConsoleKeyInfo KeyInfo)
+        private static void Error()
         {
+            Console.WriteLine("\nInvalid input. Please try again.");
+        }
+
+        private static void StartCondition()
+        {
+            bool repeat = true;
+            while (repeat == true)
+            {
+                inputChar = read.Key();
+                if (inputChar != 's')
+                {
+                    Error();
+                }
+                else
+                {
+                    Console.WriteLine("\nGameStart.");
+                    repeat = false;
+                }
+            }
+        }
+
+        private static void RepeatGame()
+        {
+            bool repeat = true;
+
+            Console.WriteLine("\nWould you like to play again?  (Y/N)");
+            while (repeat == true)
+            {
+                inputChar = read.Key();
+                if (inputChar == 'n')
+                {
+                    loopGame = false;
+                    repeat = false;
+                }
+                else if (inputChar == 'y')
+                {
+                    DefaultValues();
+                    repeat = false;
+                    Console.Clear();
+                }
+                else
+                {
+                    Error();
+                }
+            }
         }
     }
 }
