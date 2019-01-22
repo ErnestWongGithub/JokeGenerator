6e06df2af887596897cb905a2328833f143dfdd4
Ernest Wong <42599143+ErnestWongGithub@users.noreply.github.com>
2019-01-22 13:48:05 -0500

    V 0.0.1 - Starting up base of program

---
+++ JokeGeneratorConsole/UserInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JokeGenerator
+{
+    public class UserInput
+    {
+        public static object Decipher;
+        private static ConsoleKeyInfo character;
+        static char readChar;
+
+        public char Key()
+        {
+            readChar = Console.ReadKey().KeyChar;
+            
+            // escape button condition
+            if (readChar == '\u001B')
+            {
+                System.Environment.Exit(1);
+            }
+            return readChar;
+        }
+
+    }
+}
